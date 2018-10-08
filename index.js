ff6c7d2c2deb0a65b1873abdda07a3e47eac276b
Bob Polacheck <rpolacheck@gmail.com>
2018-10-08 22:48:44 +0000

    Tried to add API URL but failed

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/", function(req, res){
 app.get("/results", function(req, res){
     var query = (req.query.search);
     console.log(query);
-    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+    var url = "https://api.yelp.com/v3/businesses/search";
     request(url, function(error, response, body){
         if(!error && response.statusCode == 200) {
             var data = JSON.parse(body)
